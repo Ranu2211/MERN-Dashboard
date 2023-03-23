731209830ffb2c793cd96fb11bb21a109d6774e3
ranu2211 <ranusinghal782@gmail.com>
2023-03-23 22:53:20 +0530

    Dashboard

---
+++ server/controllers/general.js
@@ -0,0 +1,16 @@
+import User from "../models/User.js";
+
+export const getUser = async(req,res) => {
+    try{
+        const { id } = req.params;
+        console.log(id);
+        console.log("Kshitiz bro")
+        const user = await User.findById({id});
+        console.log(user)
+        res.status(200).json(user);
+
+    }
+    catch(error){
+        res.status(404).json({message: error.message})
+    }
+}
